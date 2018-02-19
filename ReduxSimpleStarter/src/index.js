1dc454c030f9fa7048d434a8ea7702dad8e6b9d4
ggodreau <godreau@gmail.com>
2018-02-19 16:25:00 -0600

    call to yt in constructor?

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search'
 
 var myKey = config.MyKey;
 
-YTSearch({ key: myKey, term: 'surfboards'}, function(data) {
-  console.log(data[0].snippet.channelTitle);
-});
+class App extends Component {
+  constructor(props){
+    super(props);
 
-const App = () => {
-  console.log(myKey);
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+    // SHOULD I BE CALLING AN API
+    // IN MY CONSTRUCTOR???!
+    YTSearch({ key: myKey, term: 'surfboards'}, (data) => {
+      console.log(data[0].snippet.channelTitle);
+      this.setState({ videos: data });
+    });
+
+    this.state = {
+      videos: []
+    };
+  }
+  render(){
+    return(
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
